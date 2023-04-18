002c0941e985f572a7140581943355f424b98845
SandyNerd <sandeepkumarmaharana1111@gmail.com>
2023-04-18 17:46:43 +0530

    alert added

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -3,21 +3,32 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextBox from './components/TextBox';
+import Alert from './components/Alert';
 
 
 
 function App() {
   const[mode, setMode]=useState('dark');  // tells whether dark mode is enabled or not
+  const[alert, setAlert]=useState(null);
+  const showAlert=(message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
+
+
   const toggleMode = () => {
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor='grey';
+      setAlert("Dark mode has been enabled.","Success")
       } 
   
     else{
       setMode('light');
       document.body.style.backgroundColor='white';
-    
+      setAlert("Light mode has been enabled.","Success")
     } 
   }
 
@@ -25,6 +36,7 @@ function App() {
   return (
     <>
 <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+<Alert Alert={alert}/>
 <div className = "container  my-3">
 <TextBox heading="Enter text to edit" mode={mode}/>
 {/* <About/> */}
